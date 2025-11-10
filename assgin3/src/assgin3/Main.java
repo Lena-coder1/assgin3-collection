4cc6f633829d37dadc3e2dd63d97c3f324b19953
lena mukhtar <lena.mukhtar@gmail.com>
2025-11-09 23:20:56 -0500

    product map

--- assgin3/src/assgin3/Main.java
+++ assgin3/src/assgin3/Main.java
@@ -10,6 +10,21 @@ class Products{
         public void run(Scanner input) {
         	int choice;
         	File file = new File("Sales.txt");
+        	
+        	Set<Integer> sales = new HashSet<>();
+        	if (file.exists()) {
+        		try(Scanner fileScanner = new Scanner(file)){
+        			while (fileScanner.hasNextLine()) {
+        				String line = fileScanner.nextLine();
+        				String[] parts = line.split(","); 
+        				int id = Integer.parseInt(parts[0].trim());
+        				sales.add(id);
+        				}
+        		}catch(Exception e) {
+        			
+        			System.out.println("error reading sales.txt file,  " + e.getMessage());
+        		}
+        	}
             do {
                 System.out.println("\n--- Prodcut Management ---");
                 System.out.println("1. update product");
@@ -21,14 +36,53 @@ class Products{
 
                 switch (choice) {
                 case 1: 
-                	System.out.println(" Please enter product code: ")
+                	System.out.println(" Please enter product code: ");
                 	int productID= input.nextInt(); 
-                	int ID = map.get(productID);
+                	input.nextLine(); 
+                	if(products.containsKey(productID) || sales.contains(productID)) {
+                		System.out.println("please enter new quantity: ");
+                		int qty = input.nextInt(); 
+                		input.nextLine();
+                		products.put(productID,qty); 
+                		System.out.println("product " + productID + " ,has been update");
+                		            			
+                	}else { 
+                		System.out.println("product ID not found.");
+                		
+                	}
+                	break;
+                	
+                case 2:
+                	System.out.println("Please enter the new product code:");
+                	int newProductID = input.nextInt();
+                	input.nextLine();
+                	if(products.containsKey(newProductID) || sales.contains(newProductID)) {
+                		System.out.println("Product ID already exists "); 
+                		
+                	}else {
+                		
+                		System.out.print("Enter quantity: ");
+                		int qty= input.nextInt(); 
+                		input.nextLine();
+                		
+                		products.put(newProductID, qty); 
+                		System.out.println("new product has been added"); 
+                		
+                		
+                	}break; 
+                	
+                case 3: 
+                	System.out.println("Exiting the product managament"); 
+                	break; 
+                	
+                default: 
+                	System.out.println("Invalid entery"); 
                 	
                 	
                 }
+               
                    
-            }
+            }while(choice !=3);
             
 
 
@@ -39,7 +93,9 @@ public class Main {
     public static void main(String[] args) {
 
 	
-    
+    Scanner input = new Scanner(System.in); 
+    Products products = new Products(); 
+    products.run(input);
     }
 
 
